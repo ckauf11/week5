3baa53f33677f5b02f7c8f1341f9735e82da59b5
CarolineKaufman <ckauf11@lsu.edu>
2018-09-27 11:37:21 -0700

    Commiting assignment

---
+++ week5.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+names="Adam Adora Cameron Caroline Charis Devin Eric Ethan Gentry Jacob Jeremy Lane Megan Peyton Ruth Willie Zara"
+
+for name in $names
+do
+echo $names
+done
+
+if [$name 
